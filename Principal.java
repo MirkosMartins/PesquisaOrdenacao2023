6505361b79dfa1cb0fb5c276a3c2439f5ad8ec6b
Eduardo Requia <101991857+Edurequia@users.noreply.github.com>
2023-08-16 21:57:58 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,69 +1,77 @@
-package source;
+package teste;
 
 public class Principal {
-	
+
 	int[] vetor = new int[10];
-	
+
 	public void mostraVetor() {
-		for(int x=0;x<vetor.length;x++) {
-			System.out.print(vetor[x]+" ");
+		for (int x = 0; x < vetor.length; x++) {
+			System.out.print(vetor[x] + " ");
 		}
-		System.out.println();
+		System.out.print("\n");
 	}
 
 	public Principal() {
-		for(int x=0;x<vetor.length;x++) {
-			this.vetor[x]=vetor.length-x;
+		for (int x = 0; x < vetor.length; x++) {
+			this.vetor[x] = vetor.length - x;
 		}
-		this.mostraVetor();
 	}
 
-	public void shakersort() {//ou COCKTAILSORT
-		int contador=0;
-		for(int z=0;z<vetor.length;z++) {
-		for(int x=0;x<vetor.length-1;x++) {
-			if(vetor[x]>vetor[x+1]) {
-				int aux = vetor[x+1];
-				vetor[x+1]=vetor[x];
-				vetor[x]=aux;
-			}
-			contador++;
-			this.mostraVetor();
-		}
-		for(int y=vetor.length-1;y>0;y--) {
-			if(vetor[y]<vetor[y-1]) {
-				int aux = vetor[y-1];
-				vetor[y-1]=vetor[y];
-				vetor[y]=aux;
-			}
-			contador++;
-			this.mostraVetor();
-		}
-		}//fim do for "z"
-		System.out.println("Contador:"+contador);
-	}
-	public void bubblesort() {
-		int t=0;
-		int contador=0;
-		for(int y=0;y<vetor.length;y++) {
-			for(int x=0;x<(vetor.length-1)-t;x++) {
-				if(vetor[x]>vetor[x+1]) {
-					int aux=vetor[x+1];
-					vetor[x+1]=vetor[x];
-					vetor[x]=aux;
+	// METODO QUE JOGA O MAIOR PRA UM LADO E O MENOR PARA OUTRO
+	public void shakersort() {
+		int s = 0;
+		int c = 0;
+		int contador = 0;
+		for (int k = 0; k < vetor.length/2 ; k++) {
+			for (int i = s; i < (vetor.length - 1) - c; i++) {
+				if (vetor[i] > vetor[i+1]) {
+					int aux = vetor[i+1];
+					vetor[i+1] = vetor[i];
+					vetor[i] = aux;
 				}
 				contador++;
 				this.mostraVetor();
 			}
+			c++;
+
+			for (int y = (vetor.length - 1)-c; y > 0 ; y--) {
+				if (vetor[y] < vetor[y-1]) {
+					int aux = vetor[y-1];
+					vetor[y-1] = vetor[y];
+					vetor[y] = aux;
+				}
+				contador++;
+				this.mostraVetor();
+			}
+			s++;
+		}
+		System.out.println("Contador: " + contador);
+	}
+
+	
+	
+	// ESSE METODO JOGA O MAIOR PRA CIMA/DIREITA
+	public void bubblesort() {
+		int t = 0;
+		int contador = 0;
+		for (int y = 0; y < vetor.length; y++) {
+			for (int x = 0; x < (vetor.length - 1) - t; x++) {
+				if (vetor[x] > vetor[x + 1]) {
+					int aux = vetor[x + 1];
+					vetor[x + 1] = vetor[x];
+					vetor[x] = aux;
+				}
+			contador++;
+				this.mostraVetor();
+			}
 			t++;
 		}
-		System.out.println("Contador:"+contador);
-		
 	}
+
 	public static void main(String[] args) {
+
 		Principal p = new Principal();
 		p.shakersort();
-
 	}
 
 }
