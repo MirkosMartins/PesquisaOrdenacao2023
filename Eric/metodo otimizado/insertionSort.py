ff7be3ef73bbf6bb1a71946c8f41668b5b06b05b
Eric Posser <eric_alegrete2002@hotmail.com>
2023-09-18 21:13:05 -0300

    Create insertionSort.py

---
+++ Eric/metodo otimizado/insertionSort.py
@@ -0,0 +1,15 @@
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+
+alist = [24,15,5,33,420,18]
+insertionSort(alist)
+print(alist)
